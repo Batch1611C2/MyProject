bcbb4af247f8edda9c442118e106f26d9ba6a0d6
Batch1611C2 <44721470+Batch1611C2@users.noreply.github.com>
2018-11-03 13:06:52 +0500

    Validation
    validation added to contact page

--- Contact.php
+++ Contact.php
@@ -15,8 +15,8 @@ if(isset($_POST['send']))
 
 
 <form name="form1" method="post">
-<p>Name: <input type="text" name="name"/> </p>
-<p>Email: <input type="text" name="email"/> </p>
+<p>Name: <input type="text" name="name" required="required"/> </p>
+<p>Email: <input type="email" name="email" required="required"/> </p>
 <p>Message: <textarea name="msg"></textarea></p>
 <p><input type="submit" name="send" value="Send" /></p>
 </form>
