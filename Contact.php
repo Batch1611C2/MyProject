03614bb3bbd1937cacddfdde25a84f6af6ebcc1d
Batch1611C2 <44721470+Batch1611C2@users.noreply.github.com>
2018-11-03 13:06:15 +0500

    ContactPage
    a contact page is addedd

---
+++ Contact.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "<p>Hello World!</p>";
+
+if(isset($_POST['send']))
+{
+	$name= $_POST['name'];
+	$email=$_POST['email'];
+	$msg=$_POST['msg'];
+	//mailng code here
+}
+
+
+?>
+
+
+<form name="form1" method="post">
+<p>Name: <input type="text" name="name"/> </p>
+<p>Email: <input type="text" name="email"/> </p>
+<p>Message: <textarea name="msg"></textarea></p>
+<p><input type="submit" name="send" value="Send" /></p>
+</form>
