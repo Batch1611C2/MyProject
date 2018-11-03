cf33327b4e16c481170fa92d35abb9d75b73879d
Batch1611C2 <44721470+Batch1611C2@users.noreply.github.com>
2018-11-03 13:02:00 +0500

    FormAdded
    a contact form is added

--- Index.php
+++ Index.php
@@ -2,3 +2,11 @@
 
 echo "<p>Hello World!</p>";
 ?>
+
+
+<form name="form1" method="post">
+<p>Name: <input type="text" name="name"/> </p>
+<p>Email: <input type="text" name="email"/> </p>
+<p>Message: <textarea name="msg"></textarea></p>
+<p><input type="submit" name="send" value="Send" /></p>
+</form>
